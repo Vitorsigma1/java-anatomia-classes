7d1886f55f083d54ffab9e88e0fd7ae528fbb648
Vitorsigma1 <vitorbuono@yahoo.com.br>
2023-08-30 22:39:05 -0300

    MinhaClasse

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,6 +1,7 @@
 public class MinhaClasse {
 public static void main (String [] args) {
-    System.out.print ( "Olá Mundo véio sem portera!");
+    System.out.println ( "Olá Mundo véio sem portera!");
+    System.out.println ("Agora eu consegui salvar no GitHub!");
 
 }
 
